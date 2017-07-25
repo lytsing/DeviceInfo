6701c73cba5699c7e6146b49bf2e3d474e160a3b
huangliqing <hlqing@gmail.com>
2017-07-25 14:24:13 +0800

    add copyright

--- src/org/lytsing/android/deviceinfo/DeviceInfoActivity.java
+++ src/org/lytsing/android/deviceinfo/DeviceInfoActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 lytsing.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lytsing.android.deviceinfo;
 
 import android.content.Intent;
@@ -16,35 +32,35 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class DeviceInfoActivity extends PreferenceActivity {
-    
+
     private final static String TAG = "DeviceInfoActivity";
-    
+
     private Preference mSdStorage;
     private Preference mProcessor;
     private Preference mMemory;
     private Preference mNetAddress;
     private Preference mMoreInfo;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.main);
-        
+
         mSdStorage = findPreference("sd_storage");
         mSdStorage.setSummary(R.string.info_not_available);
 
         mProcessor = findPreference("processor");
         mProcessor.setSummary("ARMv6-compatible processor rev 2 (v6l)");
-        
+
         mNetAddress = findPreference("net_address");
-        
+
         mMemory = findPreference("memory");
         mMoreInfo = findPreference("more_info");
-        
+
         setNetAddress();
-        
+
     }
-    
+
     private void setNetAddress() {
         String ip = getLocalIpAddress();
         if (ip != null) {
@@ -53,7 +69,7 @@ public class DeviceInfoActivity extends PreferenceActivity {
             mNetAddress.setSummary(R.string.info_not_available);
         }
     }
-    
+
     /**
      * Obtain IP address of current device
      * @return ip address
@@ -86,14 +102,13 @@ public class DeviceInfoActivity extends PreferenceActivity {
             intent.setClass(this, MarketInfoActivity.class);
             startActivity(intent);
         }
-        
+
         return false;
-        
+
     }
-    
+
     private String formatSize(long size) {
         return Formatter.formatFileSize(this, size);
     }
-
 }
 
--- src/org/lytsing/android/deviceinfo/MarketInfoActivity.java
+++ src/org/lytsing/android/deviceinfo/MarketInfoActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 lytsing.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lytsing.android.deviceinfo;
 
 import android.app.Activity;
@@ -9,6 +25,7 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
+import android.provider.Settings;
 
 import java.util.Locale;
 
@@ -17,31 +34,31 @@ import java.util.Locale;
  *
  */
 public class MarketInfoActivity extends Activity {
-    
+
     private static final String TAG = "MarketInfoActivity";
-    
+
     private String mLineNumber;
     private String mOperatorName;
     private String mOperatorNumericName;
     private String mSimOperatorName;
     private String mSimOperatorNumericName;
-    
+
     private TelephonyManager mTelephonyManager;
-    
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        
+
         setContentView(R.layout.main);
-        
+
         mTelephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         String deviceId = Settings.System.getString(getContentResolver(),
                 Settings.Secure.ANDROID_ID);
 
-        
+
         TextView tv = (TextView)findViewById(R.id.device_id);
         StringBuilder sb = new StringBuilder()
-                .append(("androidId:")
+                .append("androidId:")
                 .append(deviceId)
                 .append("\ndeviceAndSdkVersion:")
                 .append(getDeviceAndSdkVersion())
@@ -57,9 +74,9 @@ public class MarketInfoActivity extends Activity {
                 .append(getSimOperatorName())
                 .append("\nsimOperatorNumeric:")
                 .append(getSimOperatorNumericName());
-        
+
         tv.setText("Android Market collect device info:\n" + sb.toString());
-        
+
         PackageManager packageManager = getPackageManager();
         FeatureInfo[] systemAvailableFeatures = packageManager.getSystemAvailableFeatures();
 
@@ -70,19 +87,19 @@ public class MarketInfoActivity extends Activity {
                 feactures.append(arr.name).append('\n');
             }
         }
-        
+
         tv = (TextView)findViewById(R.id.android_id);
         tv.setText("SystemAvailableFeatures:\n" + feactures.toString());
     }
-    
+
     public String getLineNumber() {
         if (mLineNumber == null) {
-            mLineNumber = mTelephonyManager.getLine1Number(); 
+            mLineNumber = mTelephonyManager.getLine1Number();
         }
-        
+
         return mLineNumber;
     }
-    
+
     public String getOperatorName() {
         if (mOperatorName == null) {
             mOperatorName = mTelephonyManager.getNetworkOperatorName();
@@ -104,7 +121,7 @@ public class MarketInfoActivity extends Activity {
         }
         return mSimOperatorName;
     }
-    
+
     public String getSimOperatorNumericName() {
         if (mSimOperatorNumericName == null) {
             mSimOperatorNumericName = mTelephonyManager.getNetworkOperator();
@@ -116,18 +133,17 @@ public class MarketInfoActivity extends Activity {
     public Locale getUserLocale() {
         return Locale.getDefault();
     }
-    
+
     public String getDeviceName() {
         return Build.DEVICE;
     }
-    
+
     public String getSdkVersion() {
         return Integer.toString(Build.VERSION.SDK_INT);
     }
-    
+
     public String getDeviceAndSdkVersion() {
         return getDeviceName() + ":" + getSdkVersion();
     }
-    
 }
 
